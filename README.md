6bd1b59d1c9e06150e91c6c6731804ec64e0ed05
John Hobbs <john@velvetcache.org>
2013-05-24 17:51:24 -0500

    First version.

---
+++ README.md
@@ -0,0 +1,24 @@
+> Anyone know of a Chrome extension that keeps the current path but toggles through a list of development/test/staging domains?
+>
+> [@zachleat](https://twitter.com/zachleat/status/337631500931588096)
+
+No, but we can build one!
+
+# Status
+
+This is not complete, it's not efficient, but it works.
+
+# Usage
+
+1. Clone this repo
+1. Add it as an unpacked extension from chrome://extensions
+1. Visit the options page
+1. Add a domain set and save it
+    
+   ![example domain set](readme/domain_sets.png)
+1. Visit one of the sites in the domain set
+1. Click the button
+    
+   ![links](readme/popup.png)
+1. Use the links to jump to another domain with the same path
+1. Find some bugs, or clean up the code, then open a pull request
---
+++ popup.js
@@ -0,0 +1,53 @@
+var sets = localStorage.domain_sets;
+if( ! sets ) { 
+	$('#message').text('No Domain Sets Defined');
+}
+else {
+	sets = JSON.parse(sets);	
+
+	if( sets.length === 0 ) {
+		$('#message').text('No Domain Sets Defined');
+	}
+	else {
+		chrome.tabs.getSelected(null, function (tab){
+			var found_set
+			,   hostname
+			,   link = document.createElement("a");
+
+			link.href = tab.url;
+
+			hostname = link.hostname + (("" === link.port) ? "" : ":" + link.port);
+
+			$.each(sets, function (set_i, set) {
+				$.each(set.domains, function (domain_i, domain) {
+					if( hostname === domain ) {
+						found_set = set;
+						return false;
+					}
+				});
+				if( found_set ) {
+					return false;
+				}
+			});
+
+			if( found_set ) {
+				$('#message').text(found_set.name);
+				$.each(found_set.domains, function (i, domain) {
+					if( hostname !== domain ) {
+						$('#options').append($('<li/>').append($('<a/>').attr('href', '#').text(domain).data('domain', domain)));
+					}
+				});
+				$('a').on('click', function () {
+					var parts = $(this).data('domain').split(':');
+					link.hostname = parts[0];
+					link.port = parts[1] || '80';
+					chrome.tabs.update(tab.id, {url: link.href});
+					window.close();
+				});
+			}
+			else {
+				$('#message').text('No set for: ' + hostname);
+			}
+		});
+	}
+}
